d153d1d0ae6ca6b5081308ad6080f7a484b4ed1a
peachmakkoli <sara.lee.higgins@gmail.com>
2020-04-17 10:47:37 -0700

    renders an array of Square components via the generateSquareComponents function

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,9 +5,14 @@ import PropTypes from 'prop-types';
 
 
 const generateSquareComponents = (squares, onClickCallback) => {
-  // Complete this for Wave 1
-  // loop through squares (array of props), return a new Square component with id set to props.id and value set to props.value
-}
+  return squares.flat().map(props => 
+    <Square 
+      id={props.id} 
+      value={props.value} 
+      onClickCallback={onClickCallback} 
+    />
+  );
+};
 
 const Board = ({ squares, onClickCallback }) => {
   const squareList = generateSquareComponents(squares, onClickCallback);
@@ -15,7 +20,7 @@ const Board = ({ squares, onClickCallback }) => {
   return <div className="grid" >
     {squareList}
   </div>
-}
+};
 
 Board.propTypes = {
   squares: PropTypes.arrayOf(
