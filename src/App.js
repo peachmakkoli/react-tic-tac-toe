8dba6be20e6f8fe1249c3c61158d3d4a613b87e8
peachmakkoli <sara.lee.higgins@gmail.com>
2020-04-18 20:29:28 -0700

    refactors onClickCallback function, cleans up comments

--- src/App.js
+++ src/App.js
@@ -28,23 +28,20 @@ const generateSquares = () => {
 const App = () => {
 
   const [squares, setSquares] = useState(generateSquares());
-  const [player, setPlayer] = useState(PLAYER_1);
-  // Wave 2
-  // create onClickCallback function that changes the value of the square when it is clicked
+  const [mark, setMark] = useState(PLAYER_1);
+
   const onClickCallback = (id) => {
-    // construct a new 2D array with the value of the selected square (the one whose ID we are passing in) changed to either 'x' or 'o'
     if (id >= 0 && id <= 2) {
-      squares[0][id]['value'] = player;
+      squares[0][id]['value'] = mark; // if square is in row 1
     } else if (id >= 3 && id <= 5) {
-      squares[1][id-3]['value'] = player;
+      squares[1][id-3]['value'] = mark; // if square is in row 2
     } else if (id >= 6 && id <= 8) {
-      squares[2][id-6]['value'] = player;
+      squares[2][id-6]['value'] = mark; // if square is in row 3
     };
-    // set state logic, on click:
-    // if the player is set to 'x', set it to 'o'
-    // if the player is set to 'o', set it to 'x'
-    (player === PLAYER_1) ? setPlayer(PLAYER_2) : setPlayer(PLAYER_1);
-    setSquares(squares); // pass the new array in here
+
+    (mark === PLAYER_1) ? setMark(PLAYER_2) : setMark(PLAYER_1); // toggle 'X' or 'O'
+    
+    setSquares(squares); // updates the state
   };
 
   const checkForWinner = () => {
