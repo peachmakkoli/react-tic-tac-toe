507df7646e45be13ade09873b7eacb56d392bbd8
peachmakkoli <sara.lee.higgins@gmail.com>
2020-04-18 20:18:09 -0700

    fixes error with squares changing when they already had an X or O

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,11 +3,15 @@ import PropTypes from 'prop-types';
 
 import './Square.css'
 
-const Square = (props) => {
+const Square = (props) => {  
+  const setSquareValue = () => {
+    if (props.value === '') props.onClickCallback(props.id); // will not call onClickCallback if the square already has an 'X' or 'O'
+  };
+  
   return <button
     className="square" 
     key={props.id} 
-    onClick={() => props.onClickCallback(props.id)}
+    onClick={setSquareValue}
   >
     {props.value}
   </button>
